fb02fd50339a993925f10b1367d0fd1cd80642fe
Antoine COUSIN <ac.Acousin@free.fr>
2023-04-17 17:27:39 +0200

    form creation and implementation + bootstrap implementation

---
+++ src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\SatisfactionRatingFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_main')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(SatisfactionRatingFormType::class);
+        $form->handleRequest($request);
+
+        return $this->render('main/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
---
+++ src/Form/SatisfactionRatingFormType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SatisfactionRatingFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('score', ChoiceType::class, [
+                // 'required' => false,
+                'multiple' => false,
+                'expanded' => true,
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                ],
+            ])
+            ->add('comment', TextareaType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
